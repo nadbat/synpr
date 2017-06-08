e1ac37eeb1ad843c8e3b97b2ec366c672a4c77a5
nadbat <batalovanana@gmail.com>
2017-06-08 09:41:57 +0300

    accordeon filters

--- build/js/main.js
+++ build/js/main.js
@@ -15,3 +15,54 @@ $('.j-docs-tab').each(function() {
                 $(this).find('.b-docs__sublist').slideToggle();
             });
         });
+
+
+$('.j-content-tab').each(function() {
+ $(this).on('click', function() {
+  $(this).find('.b-content__sublist').slideToggle();
+  $(this).find('.b-content__arrow').toggleClass('is-active');
+ });
+});
+
+(function($tabs) {
+ if (!$tabs.length) {
+  return;
+ }
+ var $link = $tabs; //ссылка
+ var $tab = $('.b-content__list'); //контент
+
+ $link.on('click', function(e) {
+  var $self = $(this);
+  var index = $self.index();
+  e.preventDefault();
+  if (!$self.hasClass('is-active')) {
+   $link.removeClass('is-active');
+   $self.addClass('is-active');
+   $tab.hide();
+   $tab.eq(index).fadeIn();
+  }
+ });
+})($('.j-tabs'));
+
+$(document).on('ready', function(){
+    (function($descriptionTabs) {
+ if (!$descriptionTabs.length) {
+  return;
+ }
+ var $link = $descriptionTabs; //ссылка
+ var $tab = $('.description-content'); //контент
+
+ $link.on('click', function(e) {
+  var $self = $(this);
+  var index = $self.index();
+  e.preventDefault();
+  if (!$self.hasClass('is-active')) {
+   $link.removeClass('is-active');
+   $self.addClass('is-active');
+   $tab.hide();
+   $tab.eq(index).fadeIn();
+  }
+ });
+})($('.j-description-tab'));
+})
+
